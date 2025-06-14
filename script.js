46d924239fbd76c2c17cdcc6b6beb86e564fedd8
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-14 17:25:23 +0100

    Refactored functionality in to switch statement

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const btn = document
-  .querySelector('button')
-  .addEventListener('click', (event) => {
-    event.preventDefault();
-  });
-
 const form = document.querySelector('form');
 const pass = document.getElementById('pass');
 const passConfirm = document.getElementById('pass2');
@@ -12,34 +6,48 @@ const passConfirm = document.getElementById('pass2');
   form.addEventListener('input', (event) => {
     const target = event.target;
 
-    if (target.id === 'name') {
-      if (target.validity.patternMismatch) {
-        target.setCustomValidity('Name must be between two and four words.');
-      } else {
-        target.setCustomValidity('');
-      }
-    } else if (target.id === 'age') {
-      if (target.validity.rangeOverflow || target.validity.rangeUnderflow) {
-        target.setCustomValidity('In army there is place only for youngsters of age 18 - 25.');
-      } else {
-        target.setCustomValidity('');
-      }
-    } else if (target.id === 'post_code') {
-      if (target.validity.patternMismatch) {
-        target.setCustomValidity('Post code\'s example pattern (LV-5123)');
-      } else {
-        target.setCustomValidity('');
-      }
-    } else if (target.id === 'pass' || target.id === 'pass2') {
-      if (target.validity.patternMismatch) {
-        target.setCustomValidity('Minimum eight characters, at least one letter and one number:');
-      } else {
-        target.setCustomValidity('');
-      }
+    switch (target.id) {
+      case 'name':
+        target.setCustomValidity(
+          target.validity.patternMismatch
+            ? 'Name must be between two and four words.'
+            : ''
+        );
+        break;
+
+      case 'age':
+        target.setCustomValidity(
+          target.validity.rangeOverflow || target.validity.rangeUnderflow
+            ? 'In army there is place only for youngsters of age 18 - 25.'
+            : ''
+        );
+        break;
+
+      case 'post_code':
+        target.setCustomValidity(
+          target.validity.patternMismatch
+            ? 'Post code\'s example pattern (LV-5123)'
+            : ''
+        );
+        break;
+
+      case 'pass':
+      case 'pass2':
+        target.setCustomValidity(
+          target.validity.patternMismatch
+            ? 'Minimum eight characters, at least one letter and one number:'
+            : ''
+        );
+        break;
     }
-    if ((passConfirm.value !== pass.value) && (!pass.validity.patternMismatch && !passConfirm.validity.patternMismatch)) {
-      pass.setCustomValidity('Password don\'t match.')
+
+    if (
+      (passConfirm.value !== pass.value) &&
+      (!pass.validity.patternMismatch && !passConfirm.validity.patternMismatch)
+    ) {
+      pass.setCustomValidity('Password don\'t match.');
     }
+
     target.reportValidity();
   });
 })();
